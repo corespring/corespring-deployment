37a469425eed61d3c2c3aa6c4886b63914d5e2d4
edeustace <ed.eustace@gmail.com>
2013-08-26 13:23:11 +0200

    some tidy up

--- libs/ruby/commit-hash.rb
+++ libs/ruby/commit-hash.rb
@@ -1,28 +1,30 @@
 require_relative 'base-runner'
 
-module CommitHash
-
+class CommitHash
   include BaseRunner
+  def get(path_key, branch)
 
-  def self.get(path_key, branch)
     path = env_var path_key
     hash = ""
+    puts Dir.pwd
     in_dir(path){
       with_branch(branch){
         run_cmd "git fetch origin"
-        hash = `git rev-parse --short HEAD`
+        hash = `git rev-parse --short HEAD`.strip
       }
     }
+    puts Dir.pwd
     hash
   end
 
   private
 
-  def self.with_branch(branch, &blk)
+  def with_branch(branch, &blk)
     result = `git checkout #{branch} 2>&1`
     if result.include? "error:"
       run_cmd "git checkout -b #{branch} origin/#{branch}"
     end
     blk.call
   end
+
 end
--- libs/ruby/run-migrations.rb
+++ libs/ruby/run-migrations.rb
@@ -8,12 +8,21 @@ class RunMigrations
     jar = jar_file( env_var("LIBS_FOLDER") )
     log "jar file: #{jar}"
     migrations_path = env_var "MIGRATIONS_PATH"
+
+    # Note - the current scripts stored in the migrator have a relative path for the script name
+    # If we try and migrate from another dir - the sript name will become a full path
+    # which means that the migrator will try to re-run old migrations and fail.
+    # For now - copy the migrations over so that the relative path remains intact.
+    # We'll probably want to 1. reset the versions collection, 2. change the naming so that the root path is removed.
+    run_cmd "mkdir -p deployment"
+    run_cmd "cp -r #{migrations_path} deployment"
     mongo_uri = env_var "ENV_MONGO_URI"
     replica_set_name = env_var "ENV_MONGO_REPLICA_SET_NAME", false
     uri = get_migrator_uri(mongo_uri, replica_set_name)
 
     begin
-      run_cmd "java -jar #{jar} migrate #{commit_hash} \"#{uri}\" #{migrations_path}"
+      run_cmd "java -jar #{jar} migrate #{commit_hash} \"#{uri}\" deployment/migrations"
+      run_cmd "rm -fr deployment"
     rescue
       log "An error occured"
     end
@@ -21,7 +30,12 @@ class RunMigrations
 
   private
   def jar_file(libs_folder)
-    Dir["#{libs_folder}/*mongo-migrator*.jar"][-1]
+    puts Dir.pwd
+    puts "libs folder : #{libs_folder}"
+    puts Dir[ File.join(Dir.pwd, libs_folder)].entries
+    all = Dir["#{Dir.pwd}/#{libs_folder}/mongo-migrator*.jar"]
+    puts all
+    all[-1]
   end
 
   def get_migrator_uri(uri, replica_set_name)
