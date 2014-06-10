ef66805ae6194c0410c43dbc2b9d21f4007b8e9f
edeustace <ed.eustace@gmail.com>
2014-06-10 20:36:57 +0100

    wrap in s3 protocol name

--- libs/ruby/copy-s3-assets.rb
+++ libs/ruby/copy-s3-assets.rb
@@ -3,10 +3,14 @@ require_relative "base-runner"
 class CopyS3Assets
   include BaseRunner
 
+  def s3_bucket(name)
+    "s3://#{name}/"
+  end
+
   def run
     on_path "aws"
     target_bucket = env_var "ENV_AMAZON_ASSETS_BUCKET"
     live_bucket = env_var "CORESPRING_LIVE_ASSETS_BUCKET"
-    run_cmd "aws s3 sync --delete #{live_bucket} #{target_bucket}"
+    run_cmd "aws s3 sync --delete #{s3_bucket(live_bucket)} #{s3_bucket(target_bucket)}"
   end
 end
