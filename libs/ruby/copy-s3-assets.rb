873ecc5cebe02da805d52c088493ae7d29aada99
edeustace <ed.eustace@gmail.com>
2014-06-10 20:26:07 +0100

    use the new AWS cli instead

--- libs/ruby/copy-s3-assets.rb
+++ libs/ruby/copy-s3-assets.rb
@@ -4,14 +4,9 @@ class CopyS3Assets
   include BaseRunner
 
   def run
-    on_path "s3cmd"
-    on_path "cs-api-assets"
-    has_folder "~/cs-api-assets"
+    on_path "aws"
     target_bucket = env_var "ENV_AMAZON_ASSETS_BUCKET"
     live_bucket = env_var "CORESPRING_LIVE_ASSETS_BUCKET"
-    in_dir("~/cs-api-assets") {
-      run_cmd "cs-api-assets pull-bucket #{live_bucket}"
-      run_cmd "cs-api-assets push-bucket #{live_bucket} --remote-bucket=#{target_bucket}"
-    }
+    run_cmd "aws s3 sync --delete #{live_bucket} #{target_bucket}"
   end
 end
