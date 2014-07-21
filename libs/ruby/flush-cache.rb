4e9db318da6dcc454ff261b08997d3a3e1a24b74
dimabur <dimabur@gmail.com>
2014-07-21 15:17:06 +0300

    fixed condition to flush

--- libs/ruby/flush-cache.rb
+++ libs/ruby/flush-cache.rb
@@ -2,6 +2,8 @@
 
 require 'dalli'
 
+print 
+
 raise "Can't flush cache TARGET_APP_NAME is not defined" unless ENV.has_key?("TARGET_APP_NAME")
 
 target_app_name = ENV["TARGET_APP_NAME"]
@@ -10,7 +12,7 @@ MEMCACHIER_SERVERS = %x(heroku config:get MEMCACHIER_SERVERS -a #{target_app_nam
 MEMCACHIER_USERNAME = %x(heroku config:get MEMCACHIER_USERNAME -a #{target_app_name}).chop
 MEMCACHIER_PASSWORD = %x(heroku config:get MEMCACHIER_PASSWORD -a #{target_app_name}).chop
 
-if !MEMCACHIER_SERVERS && !MEMCACHIER_SERVERS.empty? && !MEMCACHIER_USERNAME && !MEMCACHIER_USERNAME.empty? && !MEMCACHIER_PASSWORD && !MEMCACHIER_PASSWORD.empty? 
+if MEMCACHIER_SERVERS && !MEMCACHIER_SERVERS.empty? && MEMCACHIER_USERNAME && !MEMCACHIER_USERNAME.empty? && MEMCACHIER_PASSWORD && !MEMCACHIER_PASSWORD.empty? 
     
   cache = Dalli::Client.new(MEMCACHIER_SERVERS.split(","),
   {:username => MEMCACHIER_USERNAME,
