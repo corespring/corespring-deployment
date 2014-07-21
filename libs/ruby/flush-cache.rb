8794290097dffc104dc48719e05eeb3895714482
dimabur <dimabur@gmail.com>
2014-07-21 16:50:29 +0300

    PR fixes

--- libs/ruby/flush-cache.rb
+++ libs/ruby/flush-cache.rb
@@ -2,26 +2,35 @@
 
 require 'dalli'
 
-print 
+class RunMigrations
+  include BaseRunner
 
-raise "Can't flush cache TARGET_APP_NAME is not defined" unless ENV.has_key?("TARGET_APP_NAME")
+  def run
+    raise "Can't flush cache TARGET_APP_NAME is not defined" unless ENV.has_key?("TARGET_APP_NAME")
 
-target_app_name = ENV["TARGET_APP_NAME"]
+    target_app_name = ENV["TARGET_APP_NAME"]
 
-MEMCACHIER_SERVERS = %x(heroku config:get MEMCACHIER_SERVERS -a #{target_app_name}).chop
-MEMCACHIER_USERNAME = %x(heroku config:get MEMCACHIER_USERNAME -a #{target_app_name}).chop
-MEMCACHIER_PASSWORD = %x(heroku config:get MEMCACHIER_PASSWORD -a #{target_app_name}).chop
+    memcachierServers = %x(heroku config:get MEMCACHIER_SERVERS -a #{target_app_name}).chop
+    memcachierUsername = %x(heroku config:get MEMCACHIER_USERNAME -a #{target_app_name}).chop
+    memcachierPassword = %x(heroku config:get MEMCACHIER_PASSWORD -a #{target_app_name}).chop
 
-if MEMCACHIER_SERVERS && !MEMCACHIER_SERVERS.empty? && MEMCACHIER_USERNAME && !MEMCACHIER_USERNAME.empty? && MEMCACHIER_PASSWORD && !MEMCACHIER_PASSWORD.empty? 
-    
-  cache = Dalli::Client.new(MEMCACHIER_SERVERS.split(","),
-  {:username => MEMCACHIER_USERNAME,
-   :password => MEMCACHIER_PASSWORD,
-   :failover => true,
-   :socket_timeout => 1.5,
-   :socket_failure_delay => 0.2
-  })
+    if all_present?(MEMCACHIER_SERVERS MEMCACHIER_USERNAME MEMCACHIER_PASSWORD)
+        
+      cache = Dalli::Client.new(memcachierServers.split(","),
+      {:username => memcachierUsername,
+       :password => memcachierPassword,
+       :failover => true,
+       :socket_timeout => 1.5,
+       :socket_failure_delay => 0.2
+      })
 
-  cache.flush
+      cache.flush
+
+    end
+  end
+
+  def all_present?(*strings)
+    strings.map{|v| !v.nil? and !v.empty?}.reduce{|a,b| a and b}
+  end
 
 end
